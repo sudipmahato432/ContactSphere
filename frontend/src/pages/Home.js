bab427f29a7f69e9f2bb0070ea1a6f7143d33e5e
Sudip Mahato <80632874+sudipmahato432@users.noreply.github.com>
2023-12-13 13:01:19 +0530

    Initial commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    !user && navigate("/login", { replace: true });
+  }, []);
+  return (
+    <>
+      <div className="jumbotron">
+        <h1>Welcome {user ? user.name : null}</h1>
+        <hr className="my-4" />
+        {/* <a className="btn btn-info" role="button" onClick={handleButtonClick}>
+          Add New Contacts
+        </a> */}
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
